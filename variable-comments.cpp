bd8aaf1157cea9840315f8f3fc17aa6793b2a78e
Heeda Hurain Siddiqui <heeda.hurain21@gmail.com>
2026-08-17 12:52:25 +0530

    added cpp program demonstrating variables , data types , commnets and output

---
+++ variable-comments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// This program demonstrates variables and comments
+
+int main() {
+    // Declaring and initializing variables , this is a comment
+    string name = "Ruby";
+    int age = 22;
+    float height = 5.2;
+
+    // Printing the values of variables
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " feet" << endl;
+
+    return 0;
+}
