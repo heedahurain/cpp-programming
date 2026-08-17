feb2d9b1f24d7370cecc2926f4a70e60ad8224dd
Heeda Hurain Siddiqui <heeda.hurain21@gmail.com>
2026-08-17 14:26:16 +0530

    Boolean data type example
    Demonstrates bool variables using true and false values and prints their results.

---
+++ boolean_datatype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool isStudent = true; // Declaring a boolean variable and initializing it to true
+    bool hasJob = false; // Declaring a boolean variable and initializing it to false
+
+    cout << "Is the person a student? " << isStudent << endl; // Printing the value of isStudent
+    cout << "Does the person have a job? " << hasJob << endl; // Printing the value of hasJob
+
+    return 0;
+}
