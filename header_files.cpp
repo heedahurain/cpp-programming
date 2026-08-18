aac7bba6287ffa130682e6d3c42a243fa9d0fb70
Heeda Hurain Siddiqui <heeda.hurain21@gmail.com>
2026-08-18 17:12:11 +0530

    add header files example
    demonstrating system header file

---
+++ header_files.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+//there are two types of headrer files in c++
+//1. System header files: It comes with the compiler. for example iostream, conio.h, stdlib.h etc.
+//2. User defined header files: It is written by the programmer. for example "this.h" , "myheader.h" etc. 
+
+using namespace std;
+
+int main(){
+    cout<<"this is hello world program ";
+    return 0;
+}
